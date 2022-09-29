f60d4672c4fcc71717f82e1c2465fffac21c0c1a
Adam <101269040+AdamImpactLeisure@users.noreply.github.com>
2022-09-29 17:21:50 +0200

    some changes

--- carousel.js
+++ carousel.js
@@ -14,18 +14,18 @@ function initBullets() {
         return;
     }
     const bulletContainer = document.createElement('div');
-    bulletContainer.classList.add('bullet-container')
-    slide.forEach((elem, i) => {
-        const bullet = document.createElement('div');
-        bullet.classList.add('bullet')
-        bullet.id = `bullet-index-${i}`
-        bullet.addEventListener('click', () => {
-            goToIndexSlide(i);
-        })
-        bulletContainer.appendChild(bullet);
-        elem.classList.add('proactivede');
-    })
-    container.appendChild(bulletContainer);
+    // bulletContainer.classList.add('bullet-container')
+    // slide.forEach((elem, i) => {
+    //     const bullet = document.createElement('div');
+    //     bullet.classList.add('bullet')
+    //     bullet.id = `bullet-index-${i}`
+    //     bullet.addEventListener('click', () => {
+    //         goToIndexSlide(i);
+    //     })
+    //     bulletContainer.appendChild(bullet);
+    //     elem.classList.add('proactivede');
+    // })
+    // container.appendChild(bulletContainer);
 }
 
 function initArrows() {
@@ -35,7 +35,7 @@ function initArrows() {
     const leftArrow = document.createElement('a')
     const iLeft = document.createElement('i');
     iLeft.classList.add('fa')
-    iLeft.classList.add('fa-arrow-left')
+    iLeft.classList.add('fa-chevron-left')
     iLeft.classList.add('fa-2xl')
     leftArrow.classList.add('slider-left')
     leftArrow.appendChild(iLeft)
@@ -45,7 +45,7 @@ function initArrows() {
     const rightArrow = document.createElement('a')
     const iRight = document.createElement('i');
     iRight.classList.add('fa')
-    iRight.classList.add('fa-arrow-right')
+    iRight.classList.add('fa-chevron-right')
     iRight.classList.add('fa-2xl')
     rightArrow.classList.add('slider-right')
     rightArrow.appendChild(iRight)
@@ -64,17 +64,17 @@ function slideInitial() {
     }, 500);
 }
 
-function updateBullet() {
-    if (!noBullets) {
-        document.querySelector('.bullet-container').querySelectorAll('.bullet').forEach((elem, i) => {
-            elem.classList.remove('active');
-            if (i === slideCurrent) {
-                elem.classList.add('active');
-            }
-        })
-    }
-    checkRepeat();
-}
+// function updateBullet() {
+//     if (!noBullets) {
+//         document.querySelector('.bullet-container').querySelectorAll('.bullet').forEach((elem, i) => {
+//             elem.classList.remove('active');
+//             if (i === slideCurrent) {
+//                 elem.classList.add('active');
+//             }
+//         })
+//     }
+//     checkRepeat();
+// }
 
 function checkRepeat() {
     if (!repeat) {
